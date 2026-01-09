93c4e2188240ac6d12e8718d0cc76dfa28c06981
Stepan Zubashev <szubashev@clickup.com>
2026-01-09 10:39:35 +0100

    react: hello world

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import styles from './App.module.scss'
+
+export function App() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hello, World!</h1>
+      <p className={styles.subtitle}>RA2 Maps Project</p>
+    </div>
+  )
+}
