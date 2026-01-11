2c3582847d6272f48d60c1fc3f0838175ef2d9ba
Stepan Zubashev <szubashev@clickup.com>
2026-01-11 21:01:23 +0100

    react/faiwer

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import { RefObject } from 'react';
 import { useStableCallback } from '../hooks/useStableCallback';
 import { getKeyCombo, Key } from '../utils/keyboard';
 import styles from '../App.module.scss';
@@ -7,27 +7,31 @@ interface SearchBarProps {
   value: string;
   onChange: (value: string) => void;
   onArrowDown: () => void;
+  ref: RefObject<HTMLInputElement | null>;
 }
 
-export const SearchBar = forwardRef<HTMLInputElement, SearchBarProps>(
-  ({ value, onChange, onArrowDown }, ref) => {
-    const onKeyDown = useStableCallback((e: React.KeyboardEvent) => {
-      if (getKeyCombo(e) === Key.ArrowDown) {
-        e.preventDefault();
-        onArrowDown();
-      }
-    });
+export const SearchBar = ({
+  value,
+  onChange,
+  onArrowDown,
+  ref,
+}: SearchBarProps) => {
+  const onKeyDown = useStableCallback((e: React.KeyboardEvent) => {
+    if (getKeyCombo(e) === Key.ArrowDown) {
+      e.preventDefault();
+      onArrowDown();
+    }
+  });
 
-    return (
-      <input
-        ref={ref}
-        type="text"
-        className={styles.search}
-        placeholder="Search maps..."
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        onKeyDown={onKeyDown}
-      />
-    );
-  },
-);
+  return (
+    <input
+      ref={ref}
+      type="text"
+      className={styles.search}
+      placeholder="Search maps..."
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      onKeyDown={onKeyDown}
+    />
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,16 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.scss';
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(
+  document.getElementById('root')!,
+  import.meta.env.DEV
+    ? // Vite will eliminate this section along with react/debug import.
+      {
+        testMode: true,
+        transformSource: (source) => ({
+          ...source,
+          fileName: source.fileName.replace(/^.+\/src/, location.origin),
+        }),
+      }
+    : undefined,
+).render(<App />);
